fc62e2f29fd60ac53594c43f4674296de40d2151
esra <esra@cnc.com.tr>
2022-04-02 23:39:02 +0300

    camera settings

--- level.py
+++ level.py
@@ -8,7 +8,7 @@ class Level:
     def __init__(self):
         self.display_surface = pygame.display.get_surface()
 
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = CameraGroup()
         self.active_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
 
@@ -21,12 +21,57 @@ class Level:
                 x= col_index * TILE_SIZE
                 y= row_index * TILE_SIZE
                 if col == 'X':
-                    Tile((x,y),[self.visible_sprites]) #(x,y) = pos | [self.visible_sprites] = groups --> Tile(pos, groups)
+                    Tile((x,y),[self.visible_sprites, self.collision_sprites]) #(x,y) = pos | [self.visible_sprites] = groups --> Tile(pos, groups)
                 if col == 'P':
-                    Player((x,y),[self.visible_sprites])
-
-
-
+                    self.player = Player((x,y),[self.visible_sprites, self.active_sprites], self.collision_sprites)
 
     def run(self): # draw them
-        self.visible_sprites.draw(self.display_surface)
+        self.active_sprites.update()
+        self.visible_sprites.custom_draw(self.player)
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.offset = pygame.math.Vector2(100,300 )
+
+        #center camera setup
+        # self.half_w = self.display_surface.get_size()[0] // 2
+        # self.half_h = self.display_surface.get_size()[1]//2
+
+        #camera
+        cam_top = CAMERA_BORDERS['top']
+        cam_bottom = CAMERA_BORDERS['bottom']
+        cam_left = CAMERA_BORDERS['left']
+        cam_right = CAMERA_BORDERS['right']
+        cam_width = self.display_surface.get_size()[0] - (cam_right+cam_left)
+        cam_height = self.display_surface.get_size()[1] - (cam_bottom + cam_top)
+        self.camera_rect = pygame.Rect(cam_left,cam_top,cam_width,cam_height)
+
+    def custom_draw(self,player):
+
+        #get the player offset
+        # self.offset.x = player.rect.centerx- self.half_w
+        # self.offset.y = player.rect.centery - self.half_h
+
+
+        #getting the camera position
+        if player.rect.left < self.camera_rect.left:
+            self.camera_rect.left = player.rect.left
+        if player.rect.right > self.camera_rect.right:
+            self.camera_rect.right = player.rect.right
+        if player.rect.top < self.camera_rect.top:
+            self.camera_rect.top = player.rect.top
+        if player.rect.bottom > self.camera_rect.bottom:
+            self.camera_rect.bottom = player.rect.bottom
+
+        #camera offset
+        self.offset = pygame.math.Vector2(
+            self.camera_rect.left - CAMERA_BORDERS['left'],
+            self.camera_rect.top - CAMERA_BORDERS['top']
+        )
+
+        for sprite in self.sprites():
+            offset_pos = sprite.rect.topleft -self.offset
+            self.display_surface.blit(sprite.image,offset_pos)
+
